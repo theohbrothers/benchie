998cc4470b2eff0a207348565abb2451e2cc2ef7
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2021-04-15 01:12:23 +0000

    Remove tests to verify existence or non-existence of pids

--- test/test.sh
+++ test/test.sh
@@ -48,8 +48,8 @@ for bd in $BENCHMARK_DIR $BENCHMARK_DIR_REL; do
     STDOUT=$( cat "$TEST_STDOUT_FILE" )
     echo "Expect a pid"
     echo "$STDOUT" | grep -E "[0-9]+" > "$TEST_STDOUT_FILE" && passed || failed
-    echo "Expect pid to be running"
-    kill -0 $STDOUT > /dev/null 2>&1 && passed || failed
+    # echo "Expect pid to be running"
+    # kill -0 $STDOUT > /dev/null && passed || failed
     cleanup
 
     echo
@@ -58,8 +58,8 @@ for bd in $BENCHMARK_DIR $BENCHMARK_DIR_REL; do
     STDOUT=$( cat "$TEST_STDOUT_FILE" )
     echo "Expect a pid"
     echo "$STDOUT" | grep -E "[0-9]+" > "$TEST_STDOUT_FILE" && passed || failed
-    echo "Expect pid no longer exist"
-    kill -0 $STDOUT > /dev/null 2>&1 && failed || passed
+    # echo "Expect pid no longer exist"
+    # kill -0 $STDOUT > /dev/null 2>&1 && failed || passed
     cleanup
 
     echo
